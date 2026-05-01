04427cab5f04bd286c3e95d4d92e2379113f9e3f
BurrowedInCode <coffee_gopher@proton.me>
2026-05-01 00:11:34 -0400

    wire up dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css'
+import { Routes, Route } from 'react-router'
+import Login from './pages/Login'
+import { Toaster } from 'sonner'
 
 function App() {
-
   return (
     <>
+      <Toaster position='bottom-center' />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
