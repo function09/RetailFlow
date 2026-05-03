094b37c89812cd69fccda18baf0fc600a91fb1d7
BurrowedInCode <coffee_gopher@proton.me>
2026-05-03 00:56:58 -0400

    wire up new logic to app root

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,24 @@
 import { Routes, Route } from 'react-router'
 import Login from './pages/Login'
 import { Toaster } from 'sonner'
+import { AuthProvider } from './context/AuthContext'
+import { DashBoard } from './pages/Dashboard'
+import { ProtectedRoute } from './components/ProtectedRoute'
 
 function App() {
   return (
     <>
       <Toaster position='bottom-center' />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path='/dashboard' element={
+            <ProtectedRoute>
+              <DashBoard />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </AuthProvider>
     </>
   )
 }
