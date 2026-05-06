bc8cf165012fbe1373eddc09037a2b6052f461c1
BurrowedInCode <coffee_gopher@proton.me>
2026-05-06 01:12:57 -0400

    correct scanning of address fetching

---
+++ server/internal/addresses/store.go
@@ -0,0 +1,85 @@
+package addresses
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Address struct {
+	ID          int
+	StreetLine1 string
+	StreetLine2 string
+	City        string
+	State       string
+	ZipCode     string
+	AddressType string
+	IsDefault   bool
+	CustomerID  int
+}
+
+type Store struct {
+	*sql.DB
+}
+
+type AddressStore interface {
+	GetCustomerAddresses(ctx context.Context, cid int) ([]*Address, error)
+	GetCustomerAddress(ctx context.Context, aid int) (*Address, error)
+	AddCustomerAddress(ctx context.Context, address *Address) (int, error)
+	RemoveCustomerAddress(ctx context.Context, aid int) error
+}
+
+func (s *Store) GetCustomerAddresses(ctx context.Context, cid int) ([]*Address, error) {
+	rows, err := s.QueryContext(ctx, "SELECT id, street_line_1, street_line_2, city, state, zip_code, address_type, is_default,customer_id from addresses WHERE customer_id=$1", cid)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var addresses []*Address
+
+	for rows.Next() {
+		var address Address
+
+		if err := rows.Scan(&address.ID, &address.StreetLine1, &address.StreetLine2, &address.City, &address.State, &address.ZipCode, &address.AddressType, &address.IsDefault, &address.CustomerID); err != nil {
+			return nil, err
+		}
+		addresses = append(addresses, &address)
+	}
+	return addresses, nil
+}
+
+func (s *Store) GetCustomerAddress(ctx context.Context, aid int) (*Address, error) {
+	var address Address
+	if err := s.QueryRowContext(ctx, "SELECT id, street_line_1, street_line_2, city, state, zip_code, address_type, is_default,customer_id from addresses WHERE id=$1", aid).Scan(&address.ID, &address.StreetLine1, &address.StreetLine2, &address.City, &address.State, &address.ZipCode, &address.AddressType, &address.IsDefault, &address.CustomerID); err != nil {
+		return nil, err
+	}
+	return &address, nil
+}
+
+func (s *Store) AddCustomerAddress(ctx context.Context, address *Address) (int, error) {
+	var addressID int
+	if err := s.QueryRowContext(ctx, "INSERT INTO addresses (street_line_1, street_line_2, city, state, zip_code, address_type, is_default,customer_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id", address.StreetLine1, address.StreetLine2, address.City, address.State, address.ZipCode, address.AddressType, address.IsDefault, address.CustomerID).Scan(&addressID); err != nil {
+		return 0, err
+	}
+
+	return addressID, nil
+}
+
+func (s *Store) RemoveCustomerAddress(ctx context.Context, aid int) error {
+	result, err := s.ExecContext(ctx, "DELETE from addresses WHERE id = $1", aid)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return sql.ErrNoRows
+	}
+	return nil
+}
