fc69297a318e81229152f0262ef482b206a36d3e
BurrowedInCode <coffee_gopher@proton.me>
2026-05-06 01:46:54 -0400

    fix error handling

--- server/internal/auth/service.go
+++ server/internal/auth/service.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -54,5 +55,5 @@ func ValidateToken(tokenString string, secret string) (*Claims, error) {
 
 	}
 
-	return nil, err
+	return nil, errors.New("invalid token claims")
 }
