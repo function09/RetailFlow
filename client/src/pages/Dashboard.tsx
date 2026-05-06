1ba9f588f2166f31725f2fd98c769bde08a63ecc
BurrowedInCode <coffee_gopher@proton.me>
2026-05-06 01:07:22 -0400

    create cards with filler content

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,6 +1,39 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { AuthContext } from "@/context/AuthContext"
+import { useContext } from "react"
 
 export function DashBoard() {
-  return (<div>
-    this route is protected
-  </div>)
+  const { user } = useContext(AuthContext)
+  return (
+    <div>
+
+      <div>Welcome {user?.sub}</div>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Orders</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div>45</div>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Customers</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div>21</div>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Revenue</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div>100</div>
+        </CardContent>
+      </Card>
+    </div>)
 }
