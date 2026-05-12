1dc2a6a5f3c4a04a8653fc644d660f230e0f97a8
BurrowedInCode <coffee_gopher@proton.me>
2026-05-12 03:32:11 -0400

    Change to allow checking of authorization to be passed around to other components

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -4,44 +4,46 @@ import type { User } from "../types/types"
 interface AuthContext {
   user: User | null
   loading: boolean
+  checkAuth: () => Promise<void>
 }
 
-export const AuthContext = createContext<AuthContext>({ user: null, loading: true })
+export const AuthContext = createContext<AuthContext>({ user: null, loading: true, checkAuth: async () => { } })
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<User | null>(null)
   const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
+  const checkAuth = async () => {
+    try {
 
-        const response: Response = await fetch("http://localhost:8080/auth/me", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          credentials: "include"
-        })
+      const response: Response = await fetch("http://localhost:8080/auth/me", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: "include"
+      })
 
-        if (!response.ok) {
-          setUser(null)
-          return
-        }
-        const data = await response.json()
-        setUser(data)
-      } catch (e) {
-        console.log(e)
-      } finally {
-        setLoading(false)
+      if (!response.ok) {
+        setUser(null)
+        return
       }
+      const data = await response.json()
+      setUser(data)
+    } catch (e) {
+      console.log(e)
+    } finally {
+      setLoading(false)
     }
+  }
+
+  useEffect(() => {
     checkAuth()
   }, [])
 
-
   return (
-    <AuthContext.Provider value={{ user, loading }}>
+    <AuthContext.Provider value={{ user, loading, checkAuth }}>
       {children}
     </AuthContext.Provider >
   )
+
 }
