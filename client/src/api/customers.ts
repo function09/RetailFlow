15d38de35b431814e21a9fdaa339838f8cf0bcf6
BurrowedInCode <coffee_gopher@proton.me>
2026-05-19 16:59:36 -0400

    add fetch for fetching customers orders

--- client/src/api/customers.ts
+++ client/src/api/customers.ts
@@ -1,9 +1,8 @@
-import type { Customer } from "@/types/types"
+import type { Customer, Order } from "@/types/types"
 
 const BASE_URL = "http://localhost:8080"
 
 export async function getCustomer(customerID: string): Promise<Customer> {
-
   const res = await fetch(`${BASE_URL}/customers/${customerID}`, { credentials: "include" })
 
   if (!res.ok) {
@@ -13,3 +12,12 @@ export async function getCustomer(customerID: string): Promise<Customer> {
   return res.json()
 }
 
+export async function getCustomerOrders(customerID: string): Promise<Order[]> {
+  const res = await fetch(`${BASE_URL}/customers/${customerID}/orders`, { credentials: "include" })
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch customer orders")
+  }
+
+  return res.json()
+}
