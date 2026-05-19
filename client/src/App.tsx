5361ecfc1d54e045ab7086ef683cddc7a05d42e9
BurrowedInCode <coffee_gopher@proton.me>
2026-05-19 03:11:06 -0400

    wire up react-query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Routes, Route } from 'react-router'
 import Login from './pages/Login'
 import { Toaster } from 'sonner'
@@ -9,28 +10,35 @@ import Layout from './components/Layout'
 import Products from './pages/Products'
 import Customers from './pages/Customers'
 import CustomerDetail from './pages/CustomerDetail'
+import Orders from './pages/Orders'
+
+const queryClient = new QueryClient()
+
 function App() {
   return (
     <>
-      <Toaster position='bottom-center' />
-      <AuthProvider>
-        <SidebarProvider>
-          <Routes>
-            <Route path="/login" element={<Login />} />
+      <QueryClientProvider client={queryClient}>
+        <Toaster position='bottom-center' />
+        <AuthProvider>
+          <SidebarProvider>
+            <Routes>
+              <Route path="/login" element={<Login />} />
 
-            <Route element={<ProtectedRoute />}>
-              <Route element={<Layout />}>
-                <Route path='/dashboard' element={<DashBoard />} />
-                <Route path='/orders' element={<div />} />
-                <Route path='/customers' element={<Customers />} />
-                <Route path='/customers/:customerID' element={<CustomerDetail />} />
-                <Route path='/products' element={<Products />} />
+              <Route element={<ProtectedRoute />}>
+                <Route element={<Layout />}>
+                  <Route path='/dashboard' element={<DashBoard />} />
+                  <Route path='/orders' element={<div />} />
+                  <Route path='/customers' element={<Customers />} />
+                  <Route path='/customers/:customerID' element={<CustomerDetail />} />
+                  <Route path='/products' element={<Products />} />
+                  <Route path='/orders/' element={<Orders />} />
+                </Route>
               </Route>
-            </Route>
 
-          </Routes>
-        </SidebarProvider>
-      </AuthProvider >
+            </Routes>
+          </SidebarProvider>
+        </AuthProvider >
+      </QueryClientProvider>
     </>
   )
 }
