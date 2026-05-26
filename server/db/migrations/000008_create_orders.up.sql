90d202603f2aa5a52c3b5392d8510acdbd2ee4ae
BurrowedInCode <coffee_gopher@proton.me>
2026-05-26 01:00:37 -0400

    Correct orders migration script
    wrong formatting and syntax errors observed

--- server/db/migrations/000008_create_orders.up.sql
+++ server/db/migrations/000008_create_orders.up.sql
@@ -1,15 +1,15 @@
-CREATE TYPE status AS ENUM("pending", "confirmed", "shipped", "pickedup", "delivered", "cancelled")
-CREATE TYPE fulfillment_type AS ENUM("shipping", "pickup")
+CREATE TYPE order_status AS ENUM('pending', 'confirmed', 'shipped', 'pickedup', 'delivered', 'cancelled');
+CREATE TYPE fulfillment_type AS ENUM('shipping', 'pickup');
 CREATE TABLE IF NOT EXISTS orders (
   id SERIAL PRIMARY KEY,
-  type fulfillment_type NOT NULL,
-  type status NOT NULL,
+  customer_id INT NOT NULL REFERENCES customers(id),
+  fulfillment fulfillment_type NOT NULL,
+  status order_status NOT NULL DEFAULT 'pending',
   street_line_1 TEXT,
   street_line_2 TEXT,
   city TEXT,
   state TEXT,
   zip_code TEXT,
   created_at TIMESTAMPTZ DEFAULT NOW(),
-  updated_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
 );
-
