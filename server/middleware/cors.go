e42a6a3d97a975443343ffc19a6aa12fdf489040
BurrowedInCode <coffee_gopher@proton.me>
2026-05-30 00:42:49 -0400

    change logic for CORS

--- server/middleware/cors.go
+++ server/middleware/cors.go
@@ -1,10 +1,17 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func Cors(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		origin := os.Getenv("CORS_ORIGIN")
+		if origin == "" {
+			origin = "http://localhost:5173"
+		}
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
